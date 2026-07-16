cd1446facb35b3326f1e7736836f150b8524779e
Kfnmp4h <kfnmpah321@gmail.com>
2026-07-16 19:58:38 +0200

    feat: load RoyalStory UI kit styles

--- src/main.tsx
+++ src/main.tsx
@@ -4,6 +4,8 @@ import { AuthRoot } from './AuthRoot';
 import './styles.css';
 import './reset.css';
 import './dialogs.css';
+import './ui/tokens/royal-tokens.css';
+import './ui/royal-components.css';
 import './auth/royal-auth.css';
 
 createRoot(document.getElementById('root')!).render(
