ad153f74937c179ec0796f806a5cc86d6dceaf7a
Kfnmp4h <kfnmpah321@gmail.com>
2026-07-16 14:09:37 +0200

    style: load reset dialog styles

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,7 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { AuthRoot } from './AuthRoot';
 import './styles.css';
+import './reset.css';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode><AuthRoot /></StrictMode>,
