1eb80b5420b52c5b74cee36cb7cae48b72428216
Kfnmp4h <kfnmpah321@gmail.com>
2026-07-17 09:43:59 +0200

    fix: remove duplicate logo from login panel

--- src/main.tsx
+++ src/main.tsx
@@ -5,7 +5,6 @@ import './styles.css';
 import './reset.css';
 import './dialogs.css';
 import './live-login.css';
-import './live-logo.css';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode><AuthRoot /></StrictMode>,
