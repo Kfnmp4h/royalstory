52dd0326432f8a920b48f2792f09d7f0d91c01ee
Kfnmp4h <kfnmpah321@gmail.com>
2026-07-20 12:56:05 +0200

    style: load primary tab navigation

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,7 @@ import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import { AuthRoot } from './AuthRoot';
 import './styles.css';
+import './navigation.css';
 import './reset.css';
 import './dialogs.css';
 import './live-login.css';
